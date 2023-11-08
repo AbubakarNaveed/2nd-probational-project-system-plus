bc2ebbea49e5c50c143bd57b05359164a63c7ce5
AbubakarNaveed <abubakarnaveedminhas@gmail.com>
2023-11-08 14:52:04 +0500

    project intialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthPage from "@pages/AuthPages/authPage";
 function App() {
+  // const route = createBrowserRouter([{ path: "/" }]);
   return (
     <>
-      <h1>okay</h1>
+      <AuthPage />
     </>
   );
 }
---
+++ src/pages/AuthPages/authPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const authPage = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default authPage;
